6735e1a49f1be897ace6e52725fdb10c83d793be
Martin2504 <58813528+Martin2504@users.noreply.github.com>
2023-07-03 10:40:25 +0100

    basic string manipulation

---
+++ name.py
@@ -0,0 +1,3 @@
+name = input("Name: ")      # Prompting the user to enter an input and saving it inside of a variable. 
+print("Hello, " + name)     # Concatenating 
+print(f"Hello, {name}")     # Pluging in the value of the variable string into a formatted string. 
