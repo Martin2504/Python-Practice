5cbd040a3da4a3c6be27d3a006d9339216d4f850
Martin2504 <58813528+Martin2504@users.noreply.github.com>
2023-07-03 10:54:59 +0100

    basic sequencing

---
+++ sequences.py
@@ -0,0 +1,11 @@
+name = "Harry"
+print(name[0])      # Fetch the 1st character of the string. 
+
+names = ["Harry", "Ron", "Hermione"]
+print(names)
+print(names[2])     # Fetch the 3rd element of the list. 
+
+coordinate = (10.0, 20.0)   # A touple.
+
+
+
